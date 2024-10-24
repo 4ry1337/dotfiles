24c78e48ec43e2791aa7b715f38ed5ade6c5d11d
Rakhat Yskak <yskak.rakhat@gmail.com>
2024-10-24 12:56:19 +0500

    [nvchad] update

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -7,10 +7,6 @@ local M = {}
 M.base46 = {
   theme = "tokyonight",
   transparency = true,
-  -- hl_override = {
-  --   Comment = { italic = true },
-  --   ["@comment"] = { italic = true },
-  -- },
 }
 
 return M
