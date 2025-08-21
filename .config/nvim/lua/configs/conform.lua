33e4c271a6c03bc4bc2465e6b333c0dbd963cf75
Rakhat Yskak <yskak.rakhat@gmail.com>
2025-08-21 14:43:27 +0500

    update c++ and rust

--- .config/nvim/lua/configs/conform.lua
+++ .config/nvim/lua/configs/conform.lua
@@ -4,7 +4,7 @@ return {
 		css = { "prettierd", "prettier" },
 		md = { "prettierd", "prettier" },
 		html = { "prettierd", "prettier" },
-		rust = { "rustfmt", lsp_format = "fallback" },
+		rust = { "rustfmt" },
 		sql = { "sql_formatter" },
 		c = { "clang-format" },
 		cpp = { "clang-format" },
@@ -13,6 +13,17 @@ return {
 	format_on_save = {
 		-- These options will be passed to conform.format()
 		timeout_ms = 500,
+		lsp_format = "fallback",
 		lsp_fallback = true,
+	},
+	formatters = {
+		rustfmt = {
+			options = {
+				default_edition = "2021",
+			},
+		},
+		clang_format = {
+			prepend_args = { "--style=Google" },
+		},
 	},
 }
