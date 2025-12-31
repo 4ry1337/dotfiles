055037183b26bcbaefeaff1de872bf45b931b141
Rakhat Yskak <yskak.rakhat@gmail.com>
2025-12-31 18:58:08 +0500

    [nvim] cleanup mason

--- .config/nvim/lua/configs/mason.lua
+++ .config/nvim/lua/configs/mason.lua
@@ -1,22 +1,30 @@
 return {
 	tool_installer = {
 		ensure_installed = {
+			-- lua
 			"stylua",
-			"prettierd",
-			"clangd",
-			"clang-format",
 			"lua-language-server",
+			-- C/C++
+			"clangd",
+			-- Rust
 			"rust-analyzer",
-			"sql-formatter",
+			-- Markdown
+			"marksman",
+			-- Docker
 			"dockerfile-language-server",
 			"docker-compose-language-service",
-			"buf_ls",
-			-- "typescript-language-server",
-			"gopls",
-			"codelldb",
+			-- JS/TS
 			"vtsls",
+			-- css/tailwind
 			"cssls",
 			"tailwindcss",
+			-- SQL
+			"sqlls",
+			"sql-formatter",
+			-- formatters
+			"prettierd",
+			"clang-format",
+			-- "codelldb",
 		},
 		run_on_start = true,
 		debounce_hours = 5,
