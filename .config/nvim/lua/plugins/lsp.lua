c07f6dc4d0549a0013236726b917cd2dd2452e5c
Rakhat Yskak (ary) <yskak.rakhat@gmail.com>
2026-09-20 21:30:28 +0500

    fix: removed mason from path
    Signed-off-by: Rakhat Yskak (ary) <yskak.rakhat@gmail.com>

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -13,8 +13,9 @@ return {
 		"neovim/nvim-lspconfig",
 		dependencies = { -- Automatically install LSPs and related tools to stdpath for Neovim
 			-- Mason must be loaded before its dependents so we need to set it up here.
-			-- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
-			{ "mason-org/mason.nvim", opts = {} },
+			-- Setup is called explicitly in config() below with configs.mason, not via opts here,
+			-- since opts={} would call mason.setup({}) with defaults (PATH="prepend") first.
+			{ "mason-org/mason.nvim" },
 			"mason-org/mason-lspconfig.nvim",
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
 			{ "j-hui/fidget.nvim", opts = {} },
