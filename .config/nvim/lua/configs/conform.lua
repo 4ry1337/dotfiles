d4acb0e0b858ce583228fa5fe8aee4bb894e5466
Rakhat Yskak (ary) <yskak.rakhat@gmail.com>
2026-09-21 11:48:12 +0500

    refactor: remove unnecessary
    Signed-off-by: Rakhat Yskak (ary) <yskak.rakhat@gmail.com>

--- .config/nvim/lua/configs/conform.lua
+++ .config/nvim/lua/configs/conform.lua
@@ -15,21 +15,5 @@ return {
 		typescript = { "prettierd", "prettier" },
 		typescriptreact = { "prettierd", "prettier" },
 		nix = { "nixfmt" },
-	},
-	format_on_save = {
-		timeout_ms = 500,
-	},
-	formatters = {
-		rustfmt = {
-			options = {
-				default_edition = "2021",
-			},
-		},
-		clang_format = {
-			prepend_args = { "--style=LLVM" },
-		},
-		nixfmt = {
-			args = { "-" },
-		},
-	},
+	}
 }
